e75e7b775a03a8f8cc051ddcf0ae7b297cdb72d6
Tim Kruse <coding@tim-kruse.com>
2023-11-11 13:34:01 +0100

    Added tests

--- src/test/java/io/github/mrtimeey/objectfinder/core/FindObjectUtilsTest.java
+++ src/test/java/io/github/mrtimeey/objectfinder/core/FindObjectUtilsTest.java
@@ -1,33 +1,87 @@
 package io.github.mrtimeey.objectfinder.core;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.github.mrtimeey.objectfinder.core.core.ObjectMapperFactory;
-import org.apache.commons.io.IOUtils;
+import io.github.mrtimeey.objectfinder.common.model.CalculationPart;
+import io.github.mrtimeey.objectfinder.common.model.Identifier;
+import io.github.mrtimeey.objectfinder.common.model.Offer;
+import io.github.mrtimeey.objectfinder.common.model.Property;
+import io.github.mrtimeey.objectfinder.common.TestDataFactory;
+import io.github.mrtimeey.objectfinder.core.core.FindObjectUtils;
+import io.github.mrtimeey.objectfinder.core.type.Pair;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.charset.StandardCharsets;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class FindObjectUtilsTest {
 
-   private Object offer;
-   private final ObjectMapper om = ObjectMapperFactory.objectMapper();
+   private Offer offer;
 
    @BeforeEach
-   void setup() throws IOException {
-      try (InputStream resource = this.getClass().getResourceAsStream("offer.json")) {
-         String inputObject = IOUtils.toString(resource, StandardCharsets.UTF_8);
-         offer = om.readValue(inputObject, Object.class);
-      }
+   void setup() {
+      offer = TestDataFactory.createOffer();
    }
 
    @Test
    void testFind_withIdentifier() {
-      assertThat(true).isTrue();
+      Optional<CalculationPart> result = FindObjectUtils.find(offer, Pair.of("id", Identifier.of("firstProductId_firstCalculationPart")), CalculationPart.class);
+
+      assertThat(result).isPresent();
+      assertThat(result.get().id()).isEqualTo(Identifier.of("firstProductId_firstCalculationPart"));
+      assertThat(result.get().price()).isEqualTo(BigDecimal.valueOf(11));
+   }
+
+   @Test
+   void testFind_withIdentifier_valuePath() {
+      Optional<CalculationPart> result = FindObjectUtils.find(offer, Pair.of("/id/value", "firstProductId_firstCalculationPart"), CalculationPart.class);
+
+      assertThat(result).isPresent();
+      assertThat(result.get().id()).isEqualTo(Identifier.of("firstProductId_firstCalculationPart"));
+      assertThat(result.get().price()).isEqualTo(BigDecimal.valueOf(11));
+   }
+
+   @Test
+   void testFind_withDotPath() {
+      Optional<Property> result = FindObjectUtils.find(offer, Pair.of("metadata.reference.key.value", "D"), Property.class);
+
+      assertThat(result).isPresent();
+      assertThat(result.get().id()).isEqualTo(Identifier.of("firstProductId_firstProp"));
+      assertThat(result.get().value()).isEqualTo("Lorem ipsum dolor sit amet");
+   }
+
+   @Test
+   void testFind_withMixedPath() {
+      Optional<Property> result = FindObjectUtils.find(offer, Pair.of("metadata/reference.key.value", "F"), Property.class);
+
+      assertThat(result).isPresent();
+      assertThat(result.get().id()).isEqualTo(Identifier.of("offerId_firstProp"));
+      assertThat(result.get().value()).isEqualTo("Lorem ipsum dolor sit amet");
+   }
+
+   @Test
+   void testFind_withoutMatch() {
+      Optional<Property> result = FindObjectUtils.find(offer, Pair.of("/metadata/reference/key/value", "BULLSHIT_KEY"), Property.class);
+
+      assertThat(result).isEmpty();
+   }
+
+   @Test
+   void testFindAll() {
+      List<CalculationPart> result = FindObjectUtils.findAll(offer, Pair.of("/metadata/reference/key/value", "RISK_ADDITION"), CalculationPart.class);
+
+      assertThat(result).hasSize(2);
+      assertThat(result.get(0).id()).isEqualTo(Identifier.of("firstProductId_secondCalculationPart"));
+      assertThat(result.get(1).id()).isEqualTo(Identifier.of("secondProductId_secondCalculationPart"));
+   }
+
+   @Test
+   void testFindAll_withoutMatch() {
+      List<CalculationPart> result = FindObjectUtils.findAll(offer, Pair.of("/metadata/reference/key/value", "BULLSHIT_KEY"), CalculationPart.class);
+
+      assertThat(result).isEmpty();
    }
 
 }
