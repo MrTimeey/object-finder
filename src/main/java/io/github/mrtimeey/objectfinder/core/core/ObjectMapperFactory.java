4a2bc89d9cae0dff845225d3745be4209b7b7d0f
Tim Kruse <coding@tim-kruse.com>
2023-11-11 13:33:19 +0100

    Cleanup external references

--- src/main/java/io/github/mrtimeey/objectfinder/core/core/ObjectMapperFactory.java
+++ src/main/java/io/github/mrtimeey/objectfinder/core/core/ObjectMapperFactory.java
@@ -2,16 +2,8 @@ package io.github.mrtimeey.objectfinder.core.core;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public final class ObjectMapperFactory {
 
@@ -19,32 +11,12 @@ public final class ObjectMapperFactory {
       // Hide default constructor
    }
 
-   /**
-    * Default {@link ObjectMapper} for handling the domain model.
-    *
-    * @return The configured {@link ObjectMapper}
-    */
    public static ObjectMapper objectMapper() {
       ObjectMapper mapper = new ObjectMapper();
-      enableLocalDateMapping(mapper);
-      enableOptionalMapping(mapper);
+      mapper.registerModule(new Jdk8Module());
       mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+      mapper.configure(SerializationFeature.WRITE_BIGDECIMAL_AS_PLAIN, true);
       return mapper;
    }
 
-   private static void enableOptionalMapping(ObjectMapper mapper) {
-      mapper.registerModule(new Jdk8Module());
-   }
-
-   private static void enableLocalDateMapping(ObjectMapper mapper) {
-      JavaTimeModule javaTimeModule = new JavaTimeModule();
-      DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-      DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd.MM.yyyy'T'HH:mm:ss");
-      javaTimeModule.addSerializer(new LocalDateSerializer(dateFormatter));
-      javaTimeModule.addSerializer(new LocalDateTimeSerializer(dateTimeFormatter));
-      javaTimeModule.addDeserializer(LocalDate.class, new LocalDateDeserializer(dateFormatter));
-      javaTimeModule.addDeserializer(
-            LocalDateTime.class, new LocalDateTimeDeserializer(dateTimeFormatter));
-      mapper.registerModule(javaTimeModule);
-   }
 }
